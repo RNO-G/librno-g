c50e867f294e497798f1f332167335507f503830
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2020-05-06 14:11:45 +0000

    change to 32 bits per word

--- test/throughput_test.c
+++ test/throughput_test.c
@@ -9,14 +9,18 @@
 #include <stdio.h> 
 #include <stdint.h> 
 
+#define NSAMP 2048
+#define NCHAN 24
 
-static uint16_t fake_event[24][2048]; 
+
+static uint16_t fake_event[NCHAN][NSAMP]; 
+
 
 int main(int nargs, char ** args) 
 {
 
   int fd = open ("/dev/spidev1.0",O_RDWR); 
-  int spi_clock = 48000000; 
+  int spi_clock = 480000000; 
   ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ,&spi_clock); 
 
   int ntimes = 25; 
@@ -24,13 +28,15 @@ int main(int nargs, char ** args)
 
 
 
-  struct spi_ioc_transfer xfers[24] = {0}; 
+  struct spi_ioc_transfer xfers[NCHAN] = {0}; 
+  int total_read = 0;
 
-  for (int i = 0; i< 24; i++) 
+  for (int i = 0; i< NCHAN; i++) 
   {
     xfers[i].rx_buf = (uintptr_t) &fake_event[i][0]; 
     xfers[i].tx_buf = 0; 
-    xfers[i].len = 4096; 
+    xfers[i].bits_per_word = 32; 
+    xfers[i].len = NSAMP*2; 
   }
 
 
@@ -39,13 +45,13 @@ int main(int nargs, char ** args)
   clock_gettime(CLOCK_MONOTONIC,&start);
   for (int i = 0; i < ntimes; i++) 
   {
-    ioctl(fd, SPI_IOC_MESSAGE(24), xfers); 
+    total_read += ioctl(fd, SPI_IOC_MESSAGE(NCHAN), xfers); 
   }
   clock_gettime(CLOCK_MONOTONIC,&end);
 
   double dt = (end.tv_sec - start.tv_sec) + 1e-9 * (end.tv_nsec - start.tv_nsec); 
 
-  printf("Read %d bytes (%d \"events\") in %g seconds (%g MB/s, %g Hz)\n",  24*4096*ntimes,ntimes, dt, 24*4096*ntimes/dt/(1 << 20), ntimes/dt); 
+  printf("Read %d bytes (%d \"events\") in %g seconds (%g MB/s, %g Hz)\n",  total_read,ntimes, dt, total_read/dt/(1 << 20), ntimes/dt); 
 
   close(fd); 
 
