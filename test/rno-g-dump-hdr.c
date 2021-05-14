1d1eece2d3c51b6a94ee85e1c70b53c2a175edfb
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-05-14 09:19:18 +0000

    add some dumpers

---
+++ test/rno-g-dump-hdr.c
@@ -0,0 +1,26 @@
+#include <stdlib.h> 
+#include <zlib.h> 
+#include "rno-g.h" 
+#include <stdio.h> 
+
+
+int main(int nargs, char ** args) 
+{
+
+  rno_g_header_t hd; 
+  for (int i = 1; i < nargs; i++) 
+  {
+
+    gzFile f = gzopen(args[i],"r"); 
+
+    rno_g_file_handle_t h = {.type = RNO_G_GZIP, .handle.gz = f}; 
+
+    while (rno_g_header_read(h, &hd))
+    {
+      rno_g_header_dump(stdout,&hd); 
+    }
+
+    gzclose(f); 
+  }
+
+}
---
+++ test/rno-g-dump-ped.c
@@ -0,0 +1,26 @@
+#include <stdlib.h> 
+#include <zlib.h> 
+#include "rno-g.h" 
+#include <stdio.h> 
+
+
+int main(int nargs, char ** args) 
+{
+
+  rno_g_pedestal_t wf; 
+  for (int i = 1; i <= nargs; i++) 
+  {
+
+    gzFile f = gzopen(args[i],"r"); 
+
+    rno_g_file_handle_t h = {.type = RNO_G_GZIP, .handle.gz = f}; 
+
+    while (rno_g_pedestal_read(h, &wf))
+    {
+      rno_g_pedestal_dump(stdout,&wf); 
+    }
+
+    gzclose(f); 
+  }
+
+}
