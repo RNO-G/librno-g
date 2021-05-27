5fedb68eaba687bda1bb2c2e7b0955158314e3fb
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-05-27 17:33:09 +0000

    cleanup

--- test/radiant-try-event.c
+++ test/radiant-try-event.c
@@ -6,8 +6,6 @@
 #include <unistd.h> 
 #include <time.h> 
 
-//#define ZIPPED  
-
 
 static int instrumented_poll(radiant_dev_t * rad, int timeout) 
 {
@@ -29,8 +27,12 @@ int main(int nargs, char ** args)
 {
   int N = 100; 
   int nbuffers = 2; 
+  int gzipped = 0;
   if (nargs > 1) N = atoi(args[1]); 
   if (nargs > 2) nbuffers = atoi(args[2]); 
+  if (nargs > 3) gzipped = atoi(args[3]); 
+
+
   radiant_dev_t * rad = radiant_open("/dev/spi/0.0", "/dev/ttyRadiant", 46, -61); //not sure if right gpio yet 
   if (!rad) return 1; 
 
@@ -41,14 +43,8 @@ int main(int nargs, char ** args)
 
 
 
-
-#ifdef ZIPPED
-  gzFile pf = gzopen("/data/test/peds.gz","w"); 
-  rno_g_file_handle_t ph = { .type=RNO_G_GZIP, .handle.gz = pf}; 
-#else
-  FILE * pf = fopen("/data/test/peds.dat","w"); 
-  rno_g_file_handle_t ph = { .type=RNO_G_RAW, .handle.raw = pf}; 
-#endif
+  rno_g_file_handle_t ph; 
+  rno_g_init_handle(&ph, gzipped ? "/data/test/peds.dat.gz" : "/data/test/peds.dat", "w"); 
 
 
   printf("Computing pedestals...\n");
@@ -56,63 +52,36 @@ int main(int nargs, char ** args)
   rno_g_header_t hd;
   rno_g_waveform_t wf; 
   radiant_compute_pedestals(rad, 0xffffff, 512, &ped); 
+
+  // csv for debugging purposes for now 
   FILE * fpedscsv = fopen("peds.csv","w"); 
-  rno_g_pedestal_dump(fpedscsv,  &ped); 
-  fclose(fpedscsv); 
   radiant_set_pedestals(rad,&ped); 
 
   printf("Writing out pedestals...\n");
-  rno_g_pedestal_write(ph, &ped); 
 
-#ifdef ZIPPED
-  gzclose(pf); 
-#else
-  fclose(pf);
-#endif 
+  rno_g_pedestal_dump(fpedscsv,  &ped); 
+  fclose(fpedscsv); 
+  rno_g_pedestal_write(ph, &ped); 
+  rno_g_close_handle(&ph); 
+
+
   radiant_labs_stop(rad); 
 
   //this seems to clear the extra triggers too? 
   radiant_reset_counters(rad); 
 
-
-  /*
-  radiant_dma_setup_event(rad, 0xffffff); 
-  //flush all events
-  printf("Flushing extant triggers (where did these come from? calram_zero?)\n"); 
-  int iflushed = 0; 
-  while (radiant_poll_trigger_ready(rad,500))
-  {
-    radiant_read_event(rad, &hd, &wf); 
-    printf("%d...", iflushed++); 
-    fflush(stdout); 
-  }
-  */ 
-
-
   radiant_set_nbuffers_per_readout(rad, nbuffers); 
   radiant_dma_setup_event(rad, 0xffffff); 
-
   radiant_labs_start(rad); 
 
 
-  printf("\nStarting readout using %d buffer%s!\n", nbuffers, nbuffers == 2 ? "s" : ""); 
+  printf("\nStarting %s readout using %d buffer%s!\n", gzipped ? "gzipped" : "uncompresed", nbuffers, nbuffers == 2 ? "s" : ""); 
 
-#ifdef ZIPPED
-  gzFile hf = gzopen("/data/test/header.gz","w"); 
-  rno_g_file_handle_t hh = { .type=RNO_G_GZIP, .handle.gz = hf}; 
-  gzFile ef = gzopen("/data/test/wfs.gz","w"); 
-  rno_g_file_handle_t eh = { .type=RNO_G_GZIP, .handle.gz = ef }; 
-#else
+  rno_g_file_handle_t hh;
+  rno_g_file_handle_t eh;
 
-  FILE* hf = fopen("/data/test/header.dat","w"); 
-  rno_g_file_handle_t hh = { .type=RNO_G_RAW, .handle.raw = hf}; 
-  FILE* ef = fopen("/data/test/wfs.dat","w"); 
-  rno_g_file_handle_t eh = { .type=RNO_G_RAW, .handle.raw = ef }; 
-#endif
-
-
-
-
+  rno_g_init_handle(&hh, gzipped ? "/data/test/header.dat.gz" : "/data/test/header.dat", "w");
+  rno_g_init_handle(&eh, gzipped ? "/data/test/wfs.dat.gz" : "/data/test/wfs.dat", "w");
 
   struct timespec start;
   struct timespec stop;
@@ -121,7 +90,6 @@ int main(int nargs, char ** args)
   {
     printf("====%d=====\n",i); 
 
-//    radiant_dump(rad,stdout,0); 
 
     radiant_force_trigger(rad,1,0); 
 
@@ -132,9 +100,6 @@ int main(int nargs, char ** args)
     rno_g_header_dump(stdout, &hd);
     rno_g_header_write(hh, &hd); 
     rno_g_waveform_write(eh, &wf); 
-//    rno_g_waveform_dump(stdout, &wf);
-//    rno_g_header_dump(fev, &hd);
-//    rno_g_waveform_dump(fev, &wf);
   }
 
   clock_gettime(CLOCK_REALTIME,&stop);
@@ -142,24 +107,17 @@ int main(int nargs, char ** args)
   printf("Elapsed time: %g, (%g Hz)\n",  time, N/time); 
   printf("  Note: for fastest speed, don't let this print to your terminal!!!\n"); 
 
-  // leave this at 1 buffer readout
-  radiant_set_nbuffers_per_readout(rad, 1); 
+
+  //we can stop the labs too , which by side effect also reverts to 1-buffer readout
+  radiant_labs_stop(rad); 
+
+  //set up DMA for one buffer, just in case someone else is relying on it 
   radiant_dma_setup_event(rad, 0xffffff); 
 
-//  printf("At end\n"); 
-//  radiant_dump(rad,stdout,0); 
+  rno_g_close_handle(&eh); 
+  rno_g_close_handle(&hh); 
  
-#ifdef ZIPPED
-  gzclose(ef);
-  gzclose(hf);
-#else
-  fclose(ef);
-  fclose(hf);
-#endif 
-
   radiant_close(rad); 
-
-
 }
 
 
