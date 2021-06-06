0752554fc9d7da330bcde374d8d5ac21093a2792
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-06-06 14:50:04 -0500

    prettier plot of bias scan

--- test/plotScan.C
+++ test/plotScan.C
@@ -3,11 +3,13 @@ R__LOAD_LIBRARY(libz.so)
 #include <zlib.h> 
 #include "../src/rno-g.h" 
 
-void plotScan(const char * file, int chmask = 0xffffff, int save_mode = 0) 
+void plotScan(const char * file, int chmask = 0xffffff, int save_mode = 0, int nproj = 6, int includebin=-1) 
 {
 
   gStyle->SetNumberContours(255); 
   gStyle->SetPalette(kSunset); 
+  gStyle->SetOptFit(1); 
+  gStyle->SetOptStat(0); 
   rno_g_file_handle_t h; 
   rno_g_init_handle(&h,file,"r"); 
 
@@ -65,7 +67,19 @@ void plotScan(const char * file, int chmask = 0xffffff, int save_mode = 0)
 
     if (canvi[i]) 
     {
-      canvi[i]->cd(); 
+      if (nproj) 
+      {
+
+        canvi[i]->Divide(1,2); 
+        canvi[i]->cd(1); 
+      }
+      else
+      {
+        canvi[i]->cd(); 
+      }
+
+
+      std::vector<int> proj_bins; 
 
       int nbias = vbias_l.size(); 
       double * vbias = i < RNO_G_NUM_RADIANT_CHANNELS / 2 ? &vbias_l[0] : &vbias_r[0]; 
@@ -78,10 +92,68 @@ void plotScan(const char * file, int chmask = 0xffffff, int save_mode = 0)
           h->SetBinContent(ix, ibias+1, g->GetY()[ix-1]); 
         }
       }
+
       h->SetStats(0); 
       h->SetEntries(4096*nbias); 
 
       h->Draw("colz"); 
+
+      if (nproj) 
+      {
+        gRandom->SetSeed(0); 
+        while(proj_bins.size() < nproj) 
+        {
+          int trial = gRandom->Integer(1+h->GetNbinsX()); 
+          if (proj_bins.size() ==0 && includebin >=0) trial = includebin+1; 
+          bool unique = true; 
+          for (auto x : proj_bins) 
+          {
+            if (x == trial)
+            {
+              unique = false; 
+              break; 
+            }
+          }
+          if (unique)
+          {
+            proj_bins.push_back(trial); 
+            TArrow * arr = new TArrow(h->GetXaxis()->GetBinCenter(trial), 2.7, h->GetXaxis()->GetBinCenter(trial),2.5,0.01); 
+            arr->Draw(); 
+          }
+        }
+
+        std::sort(proj_bins.begin(), proj_bins.end()); 
+      }
+
+
+
+      if (nproj) 
+      {
+        canvi[i]->cd(2)->Divide(nproj,2,0.001,0.001); 
+        for (int j = 0; j < nproj; j++) 
+        {
+          canvi[i]->cd(2)->cd(j+1); 
+
+          gStyle->SetStatX(0.5);
+          gStyle->SetStatY(0.85);
+          TH1 * proj = h->ProjectionY(Form("ch%d_bin_%d", i, proj_bins[j]), proj_bins[j], proj_bins[j]); 
+          proj->SetTitle(Form("Bin %d Projection; Bias [V]; ADC", proj_bins[j])); 
+          TF1 * fit = new TF1(Form("f%d_bin_%d",i,proj_bins[j]), "pol1",0.2,2.2); 
+          proj->Fit(fit,"RW"); 
+          proj->Draw(); 
+
+          gPad->SetGridx();
+          gPad->SetGridy();
+          canvi[i]->cd(2)->cd(nproj+j+1); 
+          TH1 * resid = new TH1D(Form("ch%d_bin_%d_resid",i,proj_bins[j]), Form("Residuals for Bin %d;Bias [V]; Linear Fit Residual [ADC]",proj_bins[j]), proj->GetNbinsX(), proj->GetXaxis()->GetXmin(), proj->GetXaxis()->GetXmax()); 
+          for (int ix = 1; ix < resid->GetNbinsX(); ix++) resid->SetBinContent(ix, proj->GetBinContent(ix)-fit->Eval(proj->GetBinCenter(ix))); 
+          resid->GetYaxis()->SetRangeUser(-60,60); 
+          resid->Draw(); 
+          gPad->SetGridx();
+          gPad->SetGridy();
+        }
+      }
+
       if (save_mode) 
         canvi[i]->SaveAs(Form("scan%d.png",i)); 
     }
