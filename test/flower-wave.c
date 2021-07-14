5e81914926257e3101382a93e106b7cf0a9d08e0
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-07-14 10:18:03 +0000

    make gain code optional

--- test/flower-wave.c
+++ test/flower-wave.c
@@ -7,21 +7,20 @@ int main (int nargs, char ** args)
 {
   flower_dev_t * flwr = flower_open("/dev/spidev1.0",-61); 
  
-  int gain_code = 0; 
-  if (nargs> 1) gain_code = atoi(args[1]); 
-
-  uint8_t codes[RNO_G_NUM_LT_CHANNELS] = {gain_code,gain_code,gain_code,gain_code}; 
+  if (nargs> 1)
+  {
+    int gain_code = atoi(args[1]); 
+    uint8_t codes[RNO_G_NUM_LT_CHANNELS] = {gain_code,gain_code,gain_code,gain_code}; 
+    printf("Gaincode = %d\n", gain_code); 
+    flower_set_gains(flwr,codes); 
+  }
 
   uint8_t data[RNO_G_NUM_LT_CHANNELS][256]; 
 //  memset(data,0x0,sizeof(data)); 
   uint8_t * data_ptr[RNO_G_NUM_LT_CHANNELS] = {&data[0][0],&data[1][0],&data[2][0],&data[3][0] }; 
 
-  printf("Gaincode = %d\n", gain_code); 
-  flower_set_gains(flwr,codes); 
-  flower_soft_trigger(flwr,0); 
-  flower_soft_trigger(flwr,1); 
+  flower_force_trigger(flwr); 
   flower_read_waveforms(flwr, 256, data_ptr); 
-  flower_soft_trigger(flwr,0); 
 
   for (int i = 0 ; i < RNO_G_NUM_LT_CHANNELS; i++) 
   {
