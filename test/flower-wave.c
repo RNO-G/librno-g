81eab86f4cbb8655b2323778184b9e29a93cc20e
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-08-17 11:34:58 -0500

    remove line returns, add timestamp

--- test/flower-wave.c
+++ test/flower-wave.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "flower.h" 
+#include <time.h> 
 #include <string.h> 
 #include <unistd.h>
 #define LEN 1024
@@ -26,6 +27,7 @@ int main (int nargs, char ** args)
   uint8_t data[RNO_G_NUM_LT_CHANNELS][LEN]; 
 //  memset(data,0x0,sizeof(data)); 
   uint8_t * data_ptr[RNO_G_NUM_LT_CHANNELS] = {&data[0][0],&data[1][0],&data[2][0],&data[3][0] }; 
+  struct timespec now; 
 
   gethostname(hostname,sizeof(hostname)); 
 
@@ -34,15 +36,17 @@ int main (int nargs, char ** args)
   {
     flower_buffer_clear(flwr); 
     if (force) flower_force_trigger(flwr); 
+    clock_gettime(CLOCK_REALTIME,&now); 
     int avail = 0; 
     while (!avail) flower_buffer_check(flwr,&avail); 
     flower_read_waveforms(flwr, LEN, data_ptr); 
 
 
     printf("%s\n\t\t{\n\t\t\t\"force\": : %s,\n", iev > 0 ? "," : "", force ? "true" : "false"); 
+    printf("\t\t\t\"when\": %09d.%09d\n", (int) now.tv_sec, (int) now.tv_nsec); 
     for (int i = 0 ; i < RNO_G_NUM_LT_CHANNELS; i++) 
     {
-      printf("\n\t\t\t\"ch%d\": [",i); 
+      printf("\t\t\t\"ch%d\": [",i); 
       for (int j = 0; j < LEN; j++) 
       {
         printf("%d",((int)data_ptr[i][j])-128); 
@@ -52,7 +56,7 @@ int main (int nargs, char ** args)
       }
       printf("]%s\n", i < RNO_G_NUM_LT_CHANNELS-1 ? "," : ""); 
     }
-    printf("\n\t\t}"); 
+    printf("\t\t}"); 
   }
   printf("\t]\n}"); 
 
