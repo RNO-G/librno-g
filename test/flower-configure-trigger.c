7d41980a400e18ce7d6ddc5ae1c97dd7897382d2
Ryan Krebs <ryankrebs016@gmail.com>
2024-03-30 12:54:08 -0400

    more test scripts

--- test/flower-configure-trigger.c
+++ test/flower-configure-trigger.c
@@ -5,9 +5,9 @@
 
 int main(int nargs, char ** args) 
 {
-  if (nargs < 4) 
+  if (nargs < 6) 
   {
-    printf("Usage: flower-configure-trigger window ncoinc vpp_mode\n"); 
+    printf("Usage: flower-configure-trigger window ncoinc vpp_mode ch_mask beam_mask\n"); 
     return 0; 
   }
  rno_g_lt_simple_trigger_config_t cfg; 
@@ -16,6 +16,9 @@ int main(int nargs, char ** args)
  cfg.window = atoi(args[1]); 
  cfg.num_coinc = atoi(args[2]); 
  cfg.vpp_mode = atoi(args[3]); 
+ cfg.channel_mask = atoi(args[4]); 
+ phased_cfg.beam_mask = atoi(args[5]); 
+
  flower_dev_t * flwr = flower_open("/dev/spidev1.0",-61); 
  flower_configure_trigger(flwr, cfg, phased_cfg); 
  flower_close(flwr); 
