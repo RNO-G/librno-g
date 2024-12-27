bd0750551d08198fb4d80f170352c56e8b5f4e10
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2024-12-26 18:50:33 -0600

    we should preprend LD_LIBRARY_PATH

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #test environment script, not for production
 DIR_PATH="$(dirname "${BASH_SOURCE[0]}")"
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${DIR_PATH}/build
+export LD_LIBRARY_PATH=${DIR_PATH}/build:$LD_LIBRARY_PATH
 export PATH=$PATH:${DIR_PATH}/build/test
 export PYTHONPATH=$PYTHONPATH:${DIR_PATH}/build
